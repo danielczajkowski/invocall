100d0fe0821f1df1026af67f556de24a25d1ff4d
Daniel Czajkowski <daniel@cut2code.com>
2023-10-18 00:08:19 +0200

    FIX: ACF page options - fixed defifiniton

--- wp-content/themes/invocall/functions.php
+++ wp-content/themes/invocall/functions.php
@@ -33,14 +33,21 @@ function register_menus()
 }
 add_action('init', 'register_menus');
 
-if (function_exists('acf_add_options_page')) {
-    $parent = acf_add_options_page(array(
-        'page_title' => 'Theme General Settings',
-        'menu_title' => 'Theme Settings',
-        'parent_slug' => 'theme-general-settings',
-        'capability' => 'edit_posts',
-        'redirect' => false
-    ));
+add_action('acf/init', 'my_acf_op_init');
+function my_acf_op_init() {
+
+    // Check function exists.
+    if( function_exists('acf_add_options_page') ) {
+
+        // Register options page.
+        $option_page = acf_add_options_page(array(
+            'page_title'    => __('Theme General Settings'),
+            'menu_title'    => __('Theme Settings'),
+            'menu_slug'     => 'acf-options-theme-settings',
+            'capability'    => 'edit_posts',
+            'redirect'      => false
+        ));
+    }
 }
 
 function create_offers()

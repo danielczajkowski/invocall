f08dd718f666ee32b772af33eab67e6bd841c54d
Daniel Czajkowski <daniel@cut2code.com>
2023-10-20 16:29:31 +0200

    FIX: Easy schame images responsives

--- wp-content/themes/invocall/sections/tiles-with-numbers/index.php
+++ wp-content/themes/invocall/sections/tiles-with-numbers/index.php
@@ -1,19 +1,31 @@
+<?php 
+    $section_title = get_sub_field('section_title') ?: '';
+    $group = get_sub_field('group') ?: array();
+
+
+    if($group) {
+    }
+
+?>
+
+<?php if( $group ): ?>
 <section class="tiles_with_numbers">
     <?php load_element_styles( 'sections/tiles-with-numbers/dist', 'tiles-with-numbers-style' ); ?>
     <div class="page--width">
-        <div class="tiles_with_numbers__wrapper">
-            <?php if( !empty( get_sub_field('section_title') ) ): ?>
-                <div class="tiles_with_numbers__left-col__wrapper">
-                    <div class="tiles_with_numbers__left-col">
-                        <h3 class="tiles_with_numbers__section-title section-title"><?php the_sub_field('section_title'); ?></h3>
+        <div class="tiles_with_numbers__wrapper offer__wrapper">
+            <?php if( $section_title ): ?>
+                <div class="tiles_with_numbers__left-col__wrapper offer__col offer__col--left">
+                    <div class="tiles_with_numbers__left-col offer__col--header">
+                        <h3 class="tiles_with_numbers__section-title section-title"><?php echo $section_title; ?></h3>
                     </div>
                 </div>
             <?php endif; ?>
-            <?php
-            $group = get_sub_field('group');
-            if( $group ): ?>
-                <h1>tiles_with_numbers</h1>
+            <?php if( $group ): ?>
+                <div class="offer__col offer__col--right">
+
+                </div>
             <?php endif; ?>
         </div>
     </div>
 </section>
+<?php endif; ?>
--- wp-content/themes/invocall/sections/two-column-per-row/index.php
+++ wp-content/themes/invocall/sections/two-column-per-row/index.php
@@ -1,19 +1,31 @@
+<?php 
+    $section_title = get_sub_field('section_title') ?: '';
+    $group = get_sub_field('group') ?: array();
+
+
+    if($group) {
+    }
+
+?>
+
+<?php if( $group ): ?>
 <section class="two_column_per_row">
     <?php load_element_styles( 'sections/two-column-per-row/dist', 'two-column-per-row-style' ); ?>
     <div class="page--width">
-        <div class="two_column_per_row__wrapper">
-            <?php if( !empty( get_sub_field('section_title') ) ): ?>
-                <div class="two_column_per_row__left-col__wrapper">
-                    <div class="two_column_per_row__left-col">
-                        <h3 class="two_column_per_row__section-title section-title"><?php the_sub_field('section_title'); ?></h3>
+        <div class="two_column_per_row__wrapper offer__wrapper">
+            <?php if( $section_title ): ?>
+                <div class="two_column_per_row__left-col__wrapper offer__col offer__col--left">
+                    <div class="two_column_per_row__left-col offer__col--header">
+                        <h3 class="two_column_per_row__section-title section-title"><?php echo $section_title; ?></h3>
                     </div>
                 </div>
             <?php endif; ?>
-            <?php
-            $group = get_sub_field('group');
-            if( $group ): ?>
-                <h1>two_column_per_row</h1>
+            <?php if( $group ): ?>
+                <div class="offer__col offer__col--right">
+
+                </div>
             <?php endif; ?>
         </div>
     </div>
 </section>
+<?php endif; ?>

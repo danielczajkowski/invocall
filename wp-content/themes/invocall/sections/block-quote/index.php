67b86beaf7730eddfd2b53fd2ab77a9b895576ca
Daniel Czajkowski <daniel@cut2code.com>
2023-10-25 01:54:21 +0200

    FEAT: new block quote

--- wp-content/themes/invocall/sections/block-quote/index.php
+++ wp-content/themes/invocall/sections/block-quote/index.php
@@ -1,35 +1,62 @@
 <?php
     $section_title = get_sub_field('heading') ?: '';
+    $group = get_sub_field('data') ?: array();
 
+    $content_top = '';
+    $tiles = array();
+    $link = array();
+
+    if($group) {
+        $content_top = $group['top_content'] ?: '';
+        $tiles = $group['repeater'] ?: array();
+        $link = $group['link'] ?: array();
+    }
 ?>
 
 <section class="blocks-quote">
-    <?php load_element_styles( 'sections/block-quote', 'block-quote-style' ); ?>
+    <?php load_element_styles( 'sections/block-quote/dist', 'block-quote-style' ); ?>
     <div class="page--width">
         <div class="blocks-quote__wrapper offer__wrapper">
-            <?php if( $section_title ): ?>
-                <div class="blocks-quote__left-col__wrapper offer__col offer__col--left">
-                    <div class="blocks-quote__left-col offer__col--header">
-                        <h3 class="blocks-quote__section-title section-title"><?php echo $section_title; ?></h3>
-                    </div>
+        <?php if( $section_title ): ?>
+            <div class="blocks-quote__left-col__wrapper offer__col offer__col--left">
+                <div class="blocks-quote__left-col offer__col--header">
+                    <h3 class="blocks-quote__section-title section-title"><?php echo $section_title; ?></h3>
                 </div>
-            <?php endif; ?>
-            <div class="offer__col offer__col--right">
-            <?php if( have_rows('repeater') ): ?>
-                <div class="blocks-quote__right-col">
-                    <?php while( have_rows('repeater') ): the_row(); ?>
-                        <blockquote class="block-quote__single">
-                            <?php if( !empty( get_sub_field('heading') ) ): ?>
-                                <em class="block-quote__heading">"<?php the_sub_field('heading'); ?>"</em>
-                            <?php endif; ?>
-                            <?php if( !empty( get_sub_field('text') ) ): ?>
-                                <div class="rte"><?php the_sub_field('text'); ?></div>
-                            <?php endif; ?>
-                        </blockquote>
-                    <?php endwhile; ?>
-                </div>
-            <?php endif; ?>
             </div>
+        <?php endif; ?>
+        <?php if( $group ): ?>
+        <div class="offer__col offer__col--right">
+        <?php if( $content_top ): ?>
+            <div class="blocks-quote__content_top content_top">
+                <?php echo $content_top; ?>
+            </div>
+        <?php endif; ?>
+        <?php if( $tiles ): ?>
+            <div class="blocks-quote__tiles">
+            <?php foreach( $tiles as $tile ):
+                $header = $tile['heading'] ?: '';
+                $content = $tile['text'] ?: '';
+            ?>
+                <?php if( $header || $content ): ?>
+                <div class="blocks-quote__tile">
+                    <?php if( $header ): ?>
+                        <blockquote class="blocks-quote__tile__header"><?php echo $header; ?></blockquote>
+                    <?php endif; ?>
+                    <?php if( $content ): ?>
+                        <div class="blocks-quote__tile__content"><?php echo $content; ?></div>
+                    <?php endif; ?>
+                </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+        <?php if( $link ): ?>
+            <div class="blocks-quote__button--wrapper">
+                <a href="<?php echo $link['url']; ?>" class="blocks-quote__button btn btn-primary"><?php echo $link['title']; ?></a>
+            </div>
+        <?php endif; ?>
+        </div>
+        <?php endif; ?>
         </div>
     </div>
 </section>
--- wp-content/themes/invocall/sections/why-we/index.php
+++ wp-content/themes/invocall/sections/why-we/index.php
@@ -2,26 +2,54 @@
     $section_title = get_sub_field('section_title') ?: '';
     $group = get_sub_field('group') ?: array();
 
+    $content_top = '';
+    $tiles = array();
+
     if($group) {
+        $content_top = $group['content-top'] ?: '';
+        $tiles = $group['tiles'] ?: array();
     }
 
 ?>
 
 <?php if( $group ): ?>
-<section class="why_we">
+<section class="why-we">
     <?php load_element_styles( 'sections/why-we/dist', 'why-we-style' ); ?>
     <div class="page--width">
-        <div class="why_we__wrapper offer__wrapper">
+        <div class="why-we__wrapper offer__wrapper">
             <?php if( $section_title ): ?>
-                <div class="why_we__left-col__wrapper offer__col offer__col--left">
-                    <div class="why_we__left-col offer__col--header">
-                        <h3 class="why_we__section-title section-title"><?php echo $section_title; ?></h3>
+                <div class="why-we__left-col__wrapper offer__col offer__col--left">
+                    <div class="why-we__left-col offer__col--header">
+                        <h3 class="why-we__section-title section-title"><?php echo $section_title; ?></h3>
                     </div>
                 </div>
             <?php endif; ?>
             <?php if( $group ): ?>
                 <div class="offer__col offer__col--right">
-
+                <?php if( $content_top ): ?>
+                    <div class="why-we__content_top content_top">
+                        <?php echo $content_top; ?>
+                    </div>
+                <?php endif; ?>
+                <?php if( $tiles ): ?>
+                    <div class="why-we__tiles">
+                    <?php foreach( $tiles as $tile ):
+                        $header = $tile['header'] ?: '';
+                        $content = $tile['content'] ?: '';
+                    ?>
+                        <?php if( $header || $content ): ?>
+                        <div class="why-we__tile">
+                            <?php if( $header ): ?>
+                                <h4 class="why-we__tile__header"><?php echo $header; ?></h4>
+                            <?php endif; ?>
+                            <?php if( $content ): ?>
+                                <div class="why-we__tile__content"><?php echo $content; ?></div>
+                            <?php endif; ?>
+                        </div>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
                 </div>
             <?php endif; ?>
         </div>
